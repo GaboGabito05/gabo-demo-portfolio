d57579cd28a61006d629488af2b65742b1c92e13
GaboGabito05 <pcjhonny13@gmail.com>
2025-03-12 07:13:34 -0500

    Cambios Aplicados

--- src/React/LikeButton.tsx
+++ src/React/LikeButton.tsx
@@ -78,9 +78,9 @@ const LikeButton = () => {
   `;
 
   return (
-    <div className="flex items-center">
+    <div className="flex items-center text-white">
       <button
-        onClick={handleLike}
+        onClick={() => window.open("https://www.upwork.com/freelancers/~0180bc0ba4ba024471", "_blank")}    
         disabled={isProcessing}
         className={`
           group relative w-40 h-10 flex items-center justify-center p-3
@@ -91,11 +91,13 @@ const LikeButton = () => {
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
+          width="16"
+          height="16"
           fill="currentColor"
-          className={svgClasses}
+          className="bi bi-person-lines-fill"
+          viewBox="0 0 16 16"
         >
-          <path d="M16.5 3C19.5376 3 22 5.5 22 9C22 16 14.5 20 12 21.5C9.5 20 2 16 2 9C2 5.5 4.5 3 7.5 3C9.35997 3 11 4 12 5C13 4 14.64 3 16.5 3ZM12.9339 18.6038C13.8155 18.0485 14.61 17.4955 15.3549 16.9029C18.3337 14.533 20 11.9435 20 9C20 6.64076 18.463 5 16.5 5C15.4241 5 14.2593 5.56911 13.4142 6.41421L12 7.82843L10.5858 6.41421C9.74068 5.56911 8.5759 5 7.5 5C5.55906 5 4 6.6565 4 9C4 11.9435 5.66627 14.533 8.64514 16.9029C9.39 17.4955 10.1845 18.0485 11.0661 18.6038C11.3646 18.7919 11.6611 18.9729 12 19.1752C12.3389 18.9729 12.6354 18.7919 12.9339 18.6038Z"></path>
+          <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1z" />
         </svg>
         <span
           className={`
@@ -103,7 +105,7 @@ const LikeButton = () => {
           text-[var(--white)]
         `}
         >
-          {likes} Likes
+          UpWork
         </span>
       </button>
     </div>
